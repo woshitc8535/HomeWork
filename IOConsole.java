0ba194dcc343f39a07584dbcee03fe07c2c143d5
woshitc8535 <47222350+woshitc8535@users.noreply.github.com>
2020-03-11 23:58:24 -0400

    Add files via upload

---
+++ IOConsole.java
@@ -0,0 +1,92 @@
+ 
+
+/**
+ * @author Yang on 01/20/2019.
+ */
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class IOConsole {
+    private final Scanner scanner;
+    private final PrintStream out;
+    
+    public IOConsole() {
+        this(System.in, System.out);
+    }
+
+    public IOConsole(InputStream inputStream, OutputStream outputStream) {
+        this(new Scanner(inputStream), new PrintStream(outputStream));
+     }
+
+    public IOConsole(Scanner scanner, PrintStream printStream) {
+       this.scanner = scanner;
+       out = printStream;
+    }
+
+    /**
+     * @param val  : text to display on console
+     * @param args : optional arguments to send for string formatting
+     */
+    public void print(String val, Object... args) {
+       out.format(val, args);
+    }
+
+    /**
+     * @param val  : text to display on console
+     * @param args : optional arguments to send for string formatting
+     */
+    public void println(String val, Object... args) {
+       out.format(val + "\n", args);
+    }
+
+    /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as String
+     */
+    public String getStringInput(String prompt, Object... args) {
+        println(prompt, args);
+        String userInput = scanner.nextLine();
+        return userInput;
+    }
+
+    /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as integer
+     */
+    public Integer getIntegerInput(String prompt, Object... args) {
+       return getLongInput(prompt, args).intValue();
+    }
+
+    /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as double
+     */
+    public Double getDoubleInput(String prompt, Object... args) {
+          return getLongInput(prompt, args).doubleValue();
+    }
+
+    /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as float
+     */
+    public Float getFloatInput(String prompt, Object... args) {
+        return scanner.nextFloat();
+    }
+
+    /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as long
+     */
+    public Long getLongInput(String prompt, Object... args) {
+         Long userNumber = scanner.nextLong();
+         return userNumber;
+    }
+}
